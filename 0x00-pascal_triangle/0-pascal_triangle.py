ab251ad2f14d7ce9ba109e4d387bd5ad59be8baf
linetgitonga <linetgitonga55@gmail.com>
2024-07-29 19:12:00 +0000

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # The first row
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # Every row ends with a 1
+        triangle.append(row)
+
+    return triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
+
